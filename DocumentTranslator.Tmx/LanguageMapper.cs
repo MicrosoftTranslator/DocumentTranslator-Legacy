11c221258415a08771a59d0ba4b5baf1bb7bfb68
Chris Wendt <christw@microsoft.com>
2015-09-07 11:17:15 -0700

    Add missing files

---
+++ DocumentTranslator.Tmx/LanguageMapper.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace Mts.Common.Tmx
+{
+    public static class LanguageMapper
+    {
+        private const string CSVFileName = "TmxLangMap.csv";
+        private static SortedDictionary<string, string> languagemap = new SortedDictionary<string, string>();
+
+        /// <summary>
+        /// Maps a language ID from one value to another value. Map is read from TmxLangMap.scv.
+        /// </summary>
+        /// <param name="langfrom">From language ID in the TMX</param>
+        /// <returns>To language, for instance the Microsoft Translator language ID</returns>
+        public static string MapLanguage(string langfrom)
+        {
+            string langto = langfrom;
+            if (! languagemap.TryGetValue(langfrom, out langto)) langto = langfrom;
+            return langto;
+        }
+
+        /// <summary>
+        /// Static initializer function. Runs once.
+        /// </summary>
+        static LanguageMapper()
+        {
+            //SortedDictionary<string, string> languagemap = new SortedDictionary<string, string>();
+            string[] lines = null;
+            try
+            {
+                lines = File.ReadAllLines(CSVFileName);
+            }
+            catch { return; }
+
+            foreach (string line in lines)
+            {
+                List<string> cells = new List<string>();
+                cells = csvParser(line);
+                try
+                {
+                    languagemap.Add(cells[0], cells[1]);
+                }
+                catch { }
+            }
+            return;
+        }
+        private static List<string> csvParser(string csv, char separator = ',')
+        {
+            List <string> parsed = new List <string>();
+            string[] temp = csv.Split(separator);
+            int counter = 0;
+            string data = string.Empty;
+            while (counter < temp.Length)
+            {
+                data = temp[counter].Trim();
+                if (data.Trim().StartsWith("\""))
+                {
+                    bool isLast = false;
+                    while (!isLast && counter < temp.Length)
+                    {
+                        data += separator.ToString() + temp[counter + 1];
+                        counter++;
+                        isLast = (temp[counter].Trim().EndsWith("\""));
+                    }
+                }
+                parsed.Add(data);
+                counter++;
+            }
+
+            return parsed;
+
+        }
+    }
+}
---
+++ DocumentTranslator.Tmx/TmxWriter.cs
@@ -0,0 +1,85 @@
+﻿//Simple TMX file writer class for producing the error log of failed sentences. 
+
+using System;
+using System.IO;
+using System.Text;
+
+namespace Mts.Common.Tmx
+{
+    public class TmxWriter : IDisposable
+    {
+        private const bool WriteToCSV = true;
+        private CsvWriter csvwriter;
+        private const string creationtool = "Microsoft Document Translator";
+        private const string creationtoolversion = "2015.09.06";
+
+
+        private StreamWriter TmxStream;
+        public enum TUError {good, lengthratio, sentencecountmismatch, tagging};
+
+        /// <summary>
+        /// Creates and initializes a TMX file for writing
+        /// </summary>
+        /// <param name="TmxFilename">TMX file name</param>
+        public TmxWriter(string filename, string sourcelanguage, string targetlanguage)
+        {
+            this.TmxStream = new StreamWriter(filename, false, Encoding.UTF8);
+            TmxWriteHeader(sourcelanguage);
+            if (WriteToCSV) {
+                this.csvwriter = new CsvWriter(Path.GetFileNameWithoutExtension(filename) + ".csv", sourcelanguage, targetlanguage);
+            }
+            return;
+        }
+
+
+        public void TmxWriteSegment(string sourcesegment, string targetsegment, string sourcelang, string targetlang, TUError tustatus)
+        {
+            this.TmxStream.WriteLine("<tu>");
+            this.TmxStream.WriteLine("<prop type=\"error\">{0}</prop>", statusmessage(tustatus));
+            this.TmxStream.WriteLine("<tuv xml:lang=\"{0}\">", sourcelang);
+            this.TmxStream.WriteLine("<seg>{0}</seg>\n</tuv>", sourcesegment);
+            this.TmxStream.WriteLine("<tu>\n<tuv xml:lang=\"{0}\">", targetlang);
+            this.TmxStream.WriteLine("<seg>{0}</seg>\n</tuv>", targetsegment);
+            this.TmxStream.WriteLine("</tu>");
+            if (WriteToCSV)
+            {
+                csvwriter.WriteSegment(sourcesegment, targetsegment, tustatus);
+            }
+        }
+
+        private string statusmessage(TUError tustatus)
+        {
+            switch (tustatus)
+            {
+                case TUError.good:
+                    return ("Good");
+                case TUError.lengthratio:
+                    return ("Length ratio exceeded");
+                case TUError.sentencecountmismatch:
+                    return ("Sentence count mismatch");
+                case TUError.tagging:
+                    return ("Sentence contains tags");
+                default:
+                    return("");
+            }
+        }
+
+        private void TmxWriteHeader(string sourcelanguage)
+        {
+            this.TmxStream.WriteLine("<tmx version=\"1.4\">");
+            this.TmxStream.WriteLine("<header\ncreationtool=\"{0}\"\ncreationtoolversion=\"{1}\"\ncreationdate=\"{2}\"\nsrclang=\"{3}\">", creationtool, creationtoolversion, DateTime.Now.ToString("yyyyMMddThhmmssZ"), sourcelanguage);
+            this.TmxStream.WriteLine("</header>");
+            this.TmxStream.WriteLine("<body>");
+        }
+
+        public void Dispose()
+        {
+            this.TmxStream.WriteLine("</body>");
+            this.TmxStream.WriteLine("</tmx>");
+            this.TmxStream.Flush();
+            this.TmxStream.Close();
+            this.TmxStream.Dispose();
+            if (WriteToCSV) { this.csvwriter.Dispose(); }
+        }
+    }
+}
