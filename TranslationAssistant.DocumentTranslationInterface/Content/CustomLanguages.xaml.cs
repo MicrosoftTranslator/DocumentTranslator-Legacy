b7caa95e562972bfc5fa8d108df9f5a620f64680
Chris Wendt <christw@microsoft.com>
2016-03-13 09:06:59 -0700

    Start of custom languages implementation.

---
+++ TranslationAssistant.DocumentTranslationInterface/Content/CustomLanguages.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TranslationAssistant.DocumentTranslationInterface.Content
+{
+    /// <summary>
+    /// Interaction logic for CustomLanguages.xaml
+    /// </summary>
+    public partial class CustomLanguages : UserControl
+    {
+        public CustomLanguages()
+        {
+            InitializeComponent();
+        }
+
+        private void CustomLanguageGrid_TargetUpdated(object sender, DataTransferEventArgs e)
+        {
+
+        }
+
+        private void CustomLanguageGrid_SourceUpdated(object sender, DataTransferEventArgs e)
+        {
+
+        }
+    }
+}
