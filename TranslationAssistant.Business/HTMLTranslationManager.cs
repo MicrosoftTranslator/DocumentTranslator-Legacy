fbe5f08a308c6c2ef53517ac915f93391b743136
Chris Wendt <christw@microsoft.com>
2016-06-07 00:00:23 -0700

    Check for null title or body.

--- TranslationAssistant.Business/HTMLTranslationManager.cs
+++ TranslationAssistant.Business/HTMLTranslationManager.cs
@@ -23,25 +23,28 @@ namespace TranslationAssistant.Business
             htmlDoc.LoadHtml(htmldocument);
             htmlDoc.DocumentNode.SetAttributeValue("lang", TranslationServices.Core.TranslationServiceFacade.LanguageNameToLanguageCode(tolanguage));
             var title = htmlDoc.DocumentNode.SelectSingleNode("//head//title");
-            title.InnerHtml = TranslationServices.Core.TranslationServiceFacade.TranslateString(title.InnerHtml, fromlanguage, tolanguage, "text/html");
+            if (title != null) title.InnerHtml = TranslationServices.Core.TranslationServiceFacade.TranslateString(title.InnerHtml, fromlanguage, tolanguage, "text/html");
             var body = htmlDoc.DocumentNode.SelectSingleNode("//body");
-            if (body.InnerHtml.Length < 10000)
+            if (body != null)
             {
-                body.InnerHtml = TranslationServices.Core.TranslationServiceFacade.TranslateString(body.InnerHtml, fromlanguage, tolanguage, "text/html");
-            }
-            else
-            {
-                List<HtmlNode> nodes = new List<HtmlNode>();
-                AddNodes(body.FirstChild, ref nodes);
+                if (body.InnerHtml.Length < 10000)
+                {
+                    body.InnerHtml = TranslationServices.Core.TranslationServiceFacade.TranslateString(body.InnerHtml, fromlanguage, tolanguage, "text/html");
+                }
+                else
+                {
+                    List<HtmlNode> nodes = new List<HtmlNode>();
+                    AddNodes(body.FirstChild, ref nodes);
 
-                Parallel.ForEach(nodes, (node) =>
-                    {
-                        if (node.InnerHtml.Length > 10000)
+                    Parallel.ForEach(nodes, (node) =>
                         {
-                            throw new Exception("Child node with a length of more than 10000 characters encountered.");
-                        }
-                        node.InnerHtml = TranslationServices.Core.TranslationServiceFacade.TranslateString(node.InnerHtml, fromlanguage, tolanguage, "text/html");
-                    });
+                            if (node.InnerHtml.Length > 10000)
+                            {
+                                throw new Exception("Child node with a length of more than 10000 characters encountered.");
+                            }
+                            node.InnerHtml = TranslationServices.Core.TranslationServiceFacade.TranslateString(node.InnerHtml, fromlanguage, tolanguage, "text/html");
+                        });
+                }
             }
             htmlDoc.Save(htmlfilename, Encoding.UTF8);
             return 1;
