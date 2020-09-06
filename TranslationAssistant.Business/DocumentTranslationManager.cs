fefd8d019e9663124361c49954ee34456a2291af
Chris Wendt <christw@microsoft.com>
2020-09-06 14:44:34 -0700

    Revert "Checkbox for aggressive normalization."
    This reverts commit d2a9b7deed97395f6ccc737b77598064fdd0b759.

--- TranslationAssistant.Business/DocumentTranslationManager.cs
+++ TranslationAssistant.Business/DocumentTranslationManager.cs
@@ -43,7 +43,6 @@ namespace TranslationAssistant.Business
     public class DocumentTranslationManager
     {
         #region Public Properties
-        public static bool AggressiveNormalization = false;
 
         #endregion
         #region Public Methods and Operators
@@ -756,12 +755,6 @@ namespace TranslationAssistant.Business
                     RemoveWebHidden = true,
                     ReplaceTabsWithSpaces = false
                 };
-                if (AggressiveNormalization)
-                {
-                    settings.NormalizeXml = true;
-                    settings.RemoveSmartTags = true;
-                }
-
                 OpenXmlPowerTools.MarkupSimplifier.SimplifyMarkup(doc, settings);
             }
 
--- TranslationAssistant.DocumentTranslationInterface/ViewModel/DocumentTranslation.cs
+++ TranslationAssistant.DocumentTranslationInterface/ViewModel/DocumentTranslation.cs
@@ -62,11 +62,6 @@ namespace TranslationAssistant.DocumentTranslationInterface.ViewModel
         /// </summary>
         private string selectedTranslateMode;
 
-        /// <summary>
-        ///     Whether to normalize aggressively.
-        /// </summary>
-        private bool aggressiveNormalization;
-
 
 
         /// <summary>
@@ -175,7 +170,7 @@ namespace TranslationAssistant.DocumentTranslationInterface.ViewModel
                             this.IsGoButtonEnabled = false;
                             this.StatusText = Properties.Resources.Common_Started;
 
-                            if (this.aggressiveNormalization) DocumentTranslationManager.AggressiveNormalization = true;
+
                             var worker = new BackgroundWorker();
                             var model = new CommentTranslationModel
                                             {
@@ -475,21 +470,6 @@ namespace TranslationAssistant.DocumentTranslationInterface.ViewModel
             }
         }
 
-        /// <summary>
-        /// Gets or sets a value indicating whether hidden content should be ignored.
-        /// </summary>
-        public bool AggressiveNormalization
-        {
-            get
-            {
-                return this.aggressiveNormalization;
-            }
-            set
-            {
-                this.aggressiveNormalization = value;
-                this.NotifyPropertyChanged("AggressiveNormalization");
-            }
-        }
 
 
 
